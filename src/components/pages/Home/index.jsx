ffab9c3818bc2ebb8bd42a57e37b88bafd39eda4
edmilson <edneto.mn@gmail.com>
2023-05-17 09:44:27 -0300

    atualizando

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,14 +1,17 @@
 import Header from "../../Header";
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 
 const Home = () => {
-    const [contador, setContador] = React.useState(0);
-    const [darkMode, setDarkMode] = React.useState(false);
+    const [contador, setContador] = useState(0);
+    const [darkMode, setDarkMode] = useState(false);
 
     const decrementar = () => {
         setContador(contador - 1)
     };
 
+   useEffect(()=>{
+    document.title = ` Contador ${contador}`
+   },[contador])
     return (
         <>  <Header />
             <div className={darkMode ? 'centro dark' : 'centro'}>
